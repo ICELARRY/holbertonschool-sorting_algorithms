d034fe70f3ada852cdf86c0c3e9acec6342a68f8
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-03 19:59:10 +0000

    Implement selection sort algorithm and add big O notation

--- sort.h
+++ sort.h
@@ -1,22 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stddef.h>  /* size_t için */
 
-/**
- * struct listint_s - Doubly linked list node
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element
- * @next: Pointer to the next element
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
-void print_list(const listint_t *list);
-void insertion_sort_list(listint_t **list);
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
